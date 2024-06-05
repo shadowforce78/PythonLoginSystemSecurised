9ac3f510c374f442d75c21adcedf1a8aa2fb3a54
shadowforce78 <planque.adam@gmail.com>
2024-06-05 23:07:14 +0200

    gnagnga

--- main_game.py
+++ main_game.py
@@ -5,7 +5,8 @@ import os
 def lobby(account):
     username = account["username"]
     os.system("cls" if os.name == "nt" else "clear")
-    print(chalk.green.bold.underline("Welcome to the game lobby, " + username + "!"))
+    print(chalk.green.bold.underline("Welcome to the lobby, " + username + "!"))
+    print("Here are the available games:")
 
 
 lobby({"username": "test"})
